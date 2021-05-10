69e120990d6524d8dbcdf6d3b526767cedc842dc
Kyle O <onghaik@gmail.com>
2021-05-10 03:12:31 -0400

    Remove incorrect nvim folder

--- nvim/lua/config/nvim-tree.lua
+++
@@ -1,3 +0,0 @@
-vim.api.nvim_set_keymap('n', '<leader>x', [[<cmd> lua require 'nvim-tree'.toggle()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>r', [[<cmd> lua require ('nvim-tree').refresh()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>n', [[<cmd> lua require ('nvim-tree').find_file()<CR>]], { noremap = true, silent = true })
--- nvim/lua/config/ultisnips.lua
+++
@@ -1,5 +0,0 @@
--- Disable all of UltiSnips's default mappings
-vim.g.UltiSnipsExpandTrigger = "<NULL>"
-vim.g.UltiSnipsListSnippets = "<NULL>"
-vim.g.UltiSnipsJumpForwardTrigger = "<NULL>"
-vim.g.UltiSnipsJumpBackwardTrigger = "<NULL>"
