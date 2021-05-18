9e41420bd49e29c12f55cb3a41106ce28d4d65d4
Kyle O <onghaik@gmail.com>
2021-05-18 00:56:01 -0400

    Add emacs.d/bin to fish_user_path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
 # Aliases
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias tlmgr='/usr/share/texmf-dist/scripts/texlive/tlmgr.pl --usermode'
+
+# Path
+fish_add_path -a ~/.emacs.d/bin
